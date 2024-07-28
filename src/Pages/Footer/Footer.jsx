75cc0bedf25fb1dc2b31f0a69d000c627b16f6d8
Abhishek Rai <90126255+raiabhi07@users.noreply.github.com>
2024-07-28 17:54:01 +0530

    Update Footer.jsx

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import profile from '../../Images/abhi.png'
 const Footer = () => {
 
     const handleButtonClick = () => {
-    window.open('https://goo.gl/maps/oDe1gkTtv1dcKM5K9', '_blank');}
+    window.open('https://maps.app.goo.gl/JRFES3TmF9oTwsKr7', '_blank');}
 
   return (
     <footer className='section-bg'>
@@ -22,7 +22,7 @@ const Footer = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-                </svg>Ghaziabad, UP
+                </svg>Bengaluru Karnataka
               </button>
             </div>
             <div className="social-links">
